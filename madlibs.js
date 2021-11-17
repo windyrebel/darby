38ae910aabf41c530183e77f290205da79ee033a
windyrebel <91164516+windyrebel@users.noreply.github.com>
2021-11-16 20:12:46 -0600

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -8,10 +8,10 @@ const thirdNoun = prompt("Type another noun.");
 const verb = prompt("Type a verb.");
 const pluralNouns = prompt("Type a plural noun.");
 
-const story = `<h2>Once upon a time, ${name} walked into a bar. The bartender nodded politely to  ${name} who took a seat on a barstool. Feeling like they needed to unwind,  ${name} asked the bartender for their favorite drink, a ${typeOfDrink}, in order to quench their ${noun}. 
+const story = `<h3>Once upon a time, ${name} walked into a bar. The bartender nodded politely to  ${name} who took a seat on a barstool. Feeling like they needed to unwind,  ${name} asked the bartender for their favorite drink, a ${typeOfDrink}, in order to quench their ${noun}. 
 The bartender thought this was an odd request, but decided to make them the ${secondNoun} that they asked for. After a few minutes, the bartender put their order on the bar.  ${name} quickly grabbed it and put it right into their mouth and then slammed the empty glass 
 on the bar top. Feeling great,  ${name} yelled out, "${thingToShout}!" at the top of their lungs. The bartender and  ${name} looked at each other and laughed at the ${adjective} exclamation.  ${name} then asked the bartender what their favorite ${thirdNoun} was and 
-they both proceeded to ${verb} about ${pluralNouns} for the rest of the afternoon.</h2>`;
+they both proceeded to ${verb} about ${pluralNouns} for the rest of the afternoon.</h3>`;
 
 document.querySelector('main').innerHTML = story;
 console.log(story)
